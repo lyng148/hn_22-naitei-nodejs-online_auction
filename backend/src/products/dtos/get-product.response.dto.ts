51dd9cba341190c91a3656b03538dbd70e8d5790
AnNgoexe <quocan2004tp@gmail.com>
2025-08-02 11:54:45 +0700

    TaskBE_GetProductById add endpoint for getting product by id

---
+++ backend/src/products/dtos/get-product.response.dto.ts
@@ -0,0 +1,64 @@
+import {
+  IsBoolean,
+  IsDate,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class ProductImageDto {
+  @IsString()
+  imageId!: string;
+
+  @IsString()
+  imageUrl!: string;
+
+  @IsBoolean()
+  isPrimary!: boolean;
+}
+
+export class ProductCategoryDto {
+  @IsString()
+  categoryId!: string;
+
+  @IsString()
+  name!: string;
+}
+
+export class ProductSellerDto {
+  @IsString()
+  userId!: string;
+
+  @IsString()
+  fullName: string | undefined;
+}
+
+export class GetProductResponseDto {
+  @IsString()
+  productId!: string;
+
+  @IsString()
+  name!: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsString()
+  status!: string;
+
+  @IsDate()
+  createdAt!: Date;
+
+  @IsDate()
+  updatedAt!: Date;
+
+  @IsNotEmpty()
+  stockQuantity!: number;
+
+  images!: ProductImageDto[];
+
+  seller!: ProductSellerDto;
+
+  categories!: ProductCategoryDto[];
+}
