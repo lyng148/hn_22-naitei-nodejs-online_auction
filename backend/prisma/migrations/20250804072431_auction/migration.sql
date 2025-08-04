82faa685c97b27d80ffb1851c8f096f66e3d3b04
AnNgoexe <quocan2004tp@gmail.com>
2025-08-04 14:26:46 +0700

    TaskBE_GetProductbyId fix endpoint

--- backend/prisma/migrations/20250804072431_auction/migration.sql
+++ backend/prisma/migrations/20250804072431_auction/migration.sql
@@ -8,9 +8,22 @@ CREATE TABLE `users` (
     `is_verified` BOOLEAN NOT NULL DEFAULT false,
     `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
     `updated_at` TIMESTAMP(0) NOT NULL,
+    `warningCount` INTEGER NOT NULL DEFAULT 0,
 
     UNIQUE INDEX `users_email_key`(`email`),
     PRIMARY KEY (`user_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `warnings` (
+    `warningId` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `adminId` VARCHAR(191) NOT NULL,
+    `reason` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`warningId`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
@@ -140,6 +153,12 @@ CREATE TABLE `payments` (
 
     PRIMARY KEY (`auction_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `warnings` ADD CONSTRAINT `warnings_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `warnings` ADD CONSTRAINT `warnings_adminId_fkey` FOREIGN KEY (`adminId`) REFERENCES `users`(`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `follows` ADD CONSTRAINT `follows_follower_id_fkey` FOREIGN KEY (`follower_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
