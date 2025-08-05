a13bcb77a2769c79c535cbfb5bb3a132578fe31a
AnNgoexe <quocan2004tp@gmail.com>
2025-08-05 16:17:45 +0700

    TaskBE_CreateAuction v2 add createAuction logic with transactional product stock updates

---
+++ backend/src/auctions/dtos/search-auction.query.dto.ts
@@ -0,0 +1,76 @@
+import {
+  IsArray,
+  IsDateString,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  IsUUID,
+  Min,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { AuctionStatus, CategoryType } from '@prisma/client';
+import { SortDirection } from '@common/types/sort-direction.enum';
+
+export class SearchAuctionQueryDto {
+  @IsOptional()
+  @IsUUID()
+  sellerId?: string;
+
+  @IsOptional()
+  @IsString()
+  title?: string;
+
+  @IsOptional()
+  @IsArray()
+  @IsEnum(CategoryType, { each: true })
+  @Type(() => String)
+  categoryType?: CategoryType[];
+
+  @IsOptional()
+  @IsEnum(AuctionStatus)
+  status?: AuctionStatus;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  minPrice?: number;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  maxPrice?: number;
+
+  @IsOptional()
+  @IsDateString()
+  startTime?: string;
+
+  @IsOptional()
+  @IsDateString()
+  endTime?: string;
+
+  @IsOptional()
+  @IsUUID()
+  hasNotId?: string;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  @Min(0)
+  page: number = 0;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  @IsPositive()
+  size: number = 10;
+
+  @IsOptional()
+  @IsString()
+  sortField: string = 'createdAt';
+
+  @IsOptional()
+  @IsEnum(SortDirection)
+  sortDirection?: SortDirection = SortDirection.DESC;
+}
