7d9266e3273ba10ed2e8668ad0e7411e93f4b27d
Nguyen-Quoc-Vuong <nguyenquocvuong161104@gmail.com>
2025-08-21 17:48:27 +0700

    Task FE ShowDetailAuction

--- frontend/src/routers/AuctionRoutes.jsx
+++ frontend/src/routers/AuctionRoutes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import CreateAuction from "@/screens/auctions/CreateAuction/index.jsx";
+import AuctionDetail from "@/screens/auctions/AuctionDetail/index.jsx";
 
 export const AuctionRoutes = () => {
     return (
         <Routes>
             <Route path="create" element={<CreateAuction />} />
+            <Route path=":auctionId" element={<AuctionDetail />} />
         </Routes>
     );
 };
